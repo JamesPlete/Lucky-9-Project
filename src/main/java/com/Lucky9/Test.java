6e1655968a1d422df93c1e39df18cd987e71cb18
Jam <jameslouisse1407@gmail.com>
2025-03-07 20:38:07 +0800

    Inayos lang 'yong ibang bugs ta's nadagdagan ng feautures. wbbbwbbiadishshi

--- src/main/java/com/Lucky9/Test.java
+++
@@ -1,6 +0,0 @@
-package com.Lucky9;
-
-public class Test {
-    public static void main (String[] args) {
-    }
-}
---
+++ src/main/java/project/Lucky9/Test.java
@@ -0,0 +1,12 @@
+package project.Lucky9;
+
+public class Test {
+    public static void main (String[] args) {
+        GameManager game = new GameManager ();
+
+        game.mainMenu();
+
+        // do'n ka na sa leaderboard na hindi nagrereflect 'yong cash takeout ta's implementing
+        // force quit ta's prompt. next is yong split kemerut.
+    }
+}
